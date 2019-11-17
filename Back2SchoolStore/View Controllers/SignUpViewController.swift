027f482f5b1ad7936d7f7f3ae4e6184ac6015372
Celine <Celineappears@gmail.com>
2019-11-17 18:23:51 -0500

    first commit =

--- Back2SchoolStore/View Controllers/SignUpViewController.swift
+++ Back2SchoolStore/View Controllers/SignUpViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var passwordAgainTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
