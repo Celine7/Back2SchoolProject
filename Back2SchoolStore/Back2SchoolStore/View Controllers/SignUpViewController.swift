cd31e4bb87dcbdfb8aa11b0d8162347ab7a3c2f4
Haoyue Cui <46913821+Celine7@users.noreply.github.com>
2019-11-28 23:01:59 -0500

    Add files via upload
    Everything~

---
+++ Back2SchoolStore/Back2SchoolStore/View Controllers/SignUpViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SignUpViewController.swift
+//  Back2SchoolStore
+//
+//  Created by mac on 2019/11/14.
+//  Copyright © 2019 NCJW. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var passwordAgainTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Back2SchoolStore/Back2SchoolStore/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Back2SchoolStore
+//
+//  Created by mac on 2019/11/14.
+//  Copyright © 2019 NCJW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
