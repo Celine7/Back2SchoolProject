45f8a4962d744e1b8d04ecccc8fe84f5fa7bf51f
Celine <Celineappears@gmail.com>
2019-11-29 00:00:37 -0500

    Adding variables for login view controller

--- Back2SchoolStore/Back2SchoolStore/View Controllers/LoginViewController.swift
+++ Back2SchoolStore/Back2SchoolStore/View Controllers/LoginViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+   
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
